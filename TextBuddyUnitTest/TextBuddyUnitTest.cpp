9a741d0fbc5933cc6aab92d5a8f3a68c1300606d
Shu Chang <shuchang.91@gmail.com>
2014-09-19 16:23:02 +0800

    UnitTested for all CE1 commands

--- TextBuddyUnitTest/TextBuddyUnitTest.cpp
+++ TextBuddyUnitTest/TextBuddyUnitTest.cpp
@@ -13,17 +13,69 @@ namespace TextBuddyUnitTest {
 			TextBuddy::setSaveFileName(input);
 
 			Assert::AreEqual(expectedOutput, TextBuddy::getSaveFileName());
-			}
+		}
 
 		TEST_METHOD(AddMessage) {
 			std::string input = "add test message";
 			std::string expectedOutput = "test message";
 			unsigned int expectedSize = 1;
 
-			TextBuddy::addText(input);
+			TextBuddy::executeInput(input);
 
 			Assert::AreEqual(expectedSize, TextBuddy::getTextStorageSize());
 			Assert::AreEqual(expectedOutput, TextBuddy::getLineFromTextStorage(0));
-			}
+		}
+
+		TEST_METHOD(DeleteMessage) {
+			std::string input = "delete 1";			
+			unsigned int expectedSize = 0;
+
+			TextBuddy::executeInput(input);
+			Assert::AreEqual(expectedSize, TextBuddy::getTextStorageSize());
+		}
+
+		TEST_METHOD(AddMultipleMessage) {
+			std::string input1 = "add first test";			
+			std::string input2 = "add second test";			
+			std::string input3 = "add third test";
+			std::string expectedOutput1 = "first test";
+			std::string expectedOutput2 = "second test";
+			std::string expectedOutput3 = "third test";
+			unsigned int expectedSize = 3;
+
+			TextBuddy::executeInput(input1);
+			TextBuddy::executeInput(input2);
+			TextBuddy::executeInput(input3);
+
+			Assert::AreEqual(expectedSize, TextBuddy::getTextStorageSize());
+			Assert::AreEqual(expectedOutput1, TextBuddy::getLineFromTextStorage(0));
+			Assert::AreEqual(expectedOutput2, TextBuddy::getLineFromTextStorage(1));
+			Assert::AreEqual(expectedOutput3, TextBuddy::getLineFromTextStorage(2));
+		}
+
+		TEST_METHOD(AddEmptyMessage) {
+			std::string input = "add   ";
+			unsigned int expectedSize = 3;
+
+			TextBuddy::executeInput(input);
+
+			Assert::AreEqual(expectedSize, TextBuddy::getTextStorageSize());
+		}
+
+		TEST_METHOD(DeleteNonexistentMessage) {
+			std::string input = "delete 5";			
+			unsigned int expectedSize = 3;
+
+			TextBuddy::executeInput(input);
+			Assert::AreEqual(expectedSize, TextBuddy::getTextStorageSize());
+		}
+
+		TEST_METHOD(ClearMessages) {
+			std::string input = "clear";			
+			unsigned int expectedSize = 0;
+
+			TextBuddy::executeInput(input);
+			Assert::AreEqual(expectedSize, TextBuddy::getTextStorageSize());
+		}
 	};
 }
